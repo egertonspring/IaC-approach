bb3c5b19a0a2ae0a99f943f3c190cfab3e2dced9
EgertonSpring <EgertonSpring@local>
2023-08-07 12:00:01 +0200

    deleted duplicates

--- 01-disable-IPv6.sh
+++
@@ -1,8 +0,0 @@
-#run with sudo
-#needed for rubygems.org to be reached
-
-echo "net.ipv6.conf.all.disable_ipv6=0" >> /etc/sysctl.conf
-echo "net.ipv6.conf.default.disable_ipv6=0" >> /etc/sysctl.conf
-echo "net.ipv6.conf.lo.disable_ipv6=0" >> /etc/sysctl.conf
-
-systemctl restart NetworkManageer
