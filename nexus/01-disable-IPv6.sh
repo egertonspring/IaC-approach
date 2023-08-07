938763e920601694115cae4fd015cc3c173fcb59
EgertonSpring <EgertonSpring@local>
2023-08-07 12:59:33 +0200

    typos

--- nexus/01-disable-IPv6.sh
+++ nexus/01-disable-IPv6.sh
@@ -5,4 +5,4 @@ echo "net.ipv6.conf.all.disable_ipv6=0" >> /etc/sysctl.conf
 echo "net.ipv6.conf.default.disable_ipv6=0" >> /etc/sysctl.conf
 echo "net.ipv6.conf.lo.disable_ipv6=0" >> /etc/sysctl.conf
 
-systemctl restart NetworkManageer
+systemctl restart NetworkManager
--- nexus/02-install-rubygems-on-ubuntu.sh
+++ nexus/02-install-rubygems-on-ubuntu.sh
@@ -1,11 +1,5 @@
 #run with sudo
 
-echo "net.ipv6.conf.all.disable_ipv6=0" >> /etc/sysctl.conf
-echo "net.ipv6.conf.default.disable_ipv6=0" >> /etc/sysctl.conf
-echo "net.ipv6.conf.lo.disable_ipv6=0" >> /etc/sysctl.conf
-
-systemctl restart NetworkManageer
-
 wget -4 https://rubygems.org/rubygems/rubygems-3.4.6.tgz
 
 tar xfv rubygems-3.4.6.tgz
